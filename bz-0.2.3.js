21c7500c01108abf69d6d30234a2a5acc271e4dc
Josh Matthews <josh@joshmatthews.net>
2014-10-20 10:58:00 -0400

    Update bz-0.2.3.js

--- bz-0.2.3.js
+++ bz-0.2.3.js
@@ -4,8 +4,8 @@ var BugzillaClient = function(options) {
   this.username = options.username;
   this.password = options.password;
   this.apiUrl = options.url || 
-    (options.test ? "https://api-dev.bugzilla.mozilla.org/test/latest/"
-                  : "https://api-dev.bugzilla.mozilla.org/latest/");
+    (options.test ? "https://bugzilla.mozilla.org/bzapi/"
+                  : "https://bugzilla.mozilla.org/bzapi/");
 }
 
 BugzillaClient.prototype = {
